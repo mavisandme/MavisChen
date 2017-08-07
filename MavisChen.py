b891652198e550c7d24470bb30791522666fccf5
mavisandme <30542665+mavisandme@users.noreply.github.com>
2017-08-07 08:08:12 +0800

    ver2

--- MavisChen.py
+++ MavisChen.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python
 #coding:utf-8
 
-from flask import Flask,flash,render_template,request
+from flask import Flask,flash,render_template,request,url_for
+
 
 app = Flask(__name__)
 
 
-@app.route('/index')
+@app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/project')
-def project():
-    return render_template('project.html')
+# @app.route('/project')
+# def project():
+#     return render_template('project.html')
+#
+# @app.route('/education')
+# def education():
+#     return render_template('education.html')
+#
+# @app.route('/skill')
+# def skill():
+#     return render_template('skill.html')
+#
+# @app.route('/recommendation')
+# def recommendation():
+#     return render_template('recommendation.html')
 
-@app.route('/education')
-def education():
-    return render_template('education.html')
-
-@app.route('/skill')
-def skill():
-    return render_template('skill.html')
-
-@app.route('/recommendation')
-def recommendation():
-    return render_template('recommendation.html')
 
 
 
--- static/resume.js
+++ static/resume.js
@@ -6,11 +6,11 @@ $(document).ready(function () {
     $('#showMe1').click(function () {
 
        if( $(this).text() == "Show Me" ){
-           $("#img1 img").slideDown(2000);
+           $("#img1 img,#img1 div").slideDown(2000);
            $('#showMe1').text('Hide Me');
        }
        else {
-           $('#img1 img').slideUp(2000);
+           $('#img1 img,#img1 div').slideUp(2000);
            $('#showMe1').text('Show Me');
        }
     });
@@ -19,11 +19,11 @@ $(document).ready(function () {
     $('#showMe2').click(function () {
 
         if( $(this).text() == "Show Me" ){
-            $("#img2 img").slideDown(2000);
+            $("#img2 img,#img2 div").slideDown(2000);
             $('#showMe2').text('Hide Me');
         }
         else {
-            $('#img2 img').slideUp(2000);
+            $('#img2 img,#img2 div').slideUp(2000);
             $('#showMe2').text('Show Me');
         }
     });
@@ -44,5 +44,60 @@ $(document).ready(function () {
     $('#menuContainer img').click(function () {
        $('#menuContainer ul').slideToggle(100);
     });
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop()>=50){
+            $('#btn_top').fadeIn();
+        }
+        else {
+            $('#btn_top').fadeOut();
+        }
+    });
+
+    //回到顶部
+    $('#btn_top').click(function () {
+        $('html,body').animate({scrollTop:0},500);
+    });
+
+
+    //回到自我介绍
+    $("#menuItem-1990").mouseenter(function () {
+        // $('#menuItem-1990').css()
+        var h1 = $('#person').offset().top - 50;
+        $('html,body').animate({scrollTop:h1},500);
+
+    });
+
+    //回到工作项目
+    $('#menuItem-1991').mouseenter(function () {
+        var h2 = $('#workProject').offset().top - 50;
+        $('html,body').animate({scrollTop:h2},500);
+    });
+
+    //回到工作和教育
+    $('#menuItem-1992').mouseenter(function () {
+        var h3 = $('#education').offset().top - 50;
+        $('html,body').animate({scrollTop:h3},500);
+    });
+
+    //回到技能与兴趣
+    $('#menuItem-1993').mouseenter(function () {
+        var h4 = $('#skillAndHobby').offset().top - 50;
+        $('html,body').animate({scrollTop:h4},500);
+    });
+
+    //回到自我推荐
+    $('#menuItem-1994').mouseenter(function () {
+        var h5 = $('#recommendation').offset().top -50;
+        $('html,body').animate({scrollTop:h5},500);
+    });
+
+
+
+
+
+
+
+
 
 });
